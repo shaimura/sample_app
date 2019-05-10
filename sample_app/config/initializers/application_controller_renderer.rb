7195cac9e02b803f83901d11f8c3d4483c5c59bc
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-05-11 01:16:45 +0900

    [finish]rails8章

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '30de55dea91934ca0f0b2a7c24151d08c560c04d413101b367d2420a36bef27604d8e67f1b817b6ea691bde554100d3da569712cf7f6c3be2d5ffdcd1918b5d0'
